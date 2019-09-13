e7a4be9a9cf62fab65708ea3315e1ed8c53a7fd0
そな <lite01250@gmail.com>
2019-09-13 13:58:08 +0900

    TitleSceneからMainSceneに変移可能に

---
+++ Assets/Scripts/System/SceneManager/TitleSceneManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _GameManagerObject = null;
+    private GameManager _GameManager = null;
+
+    void Start()
+    {
+        //GameManagerが存在しなければ生成する
+        if (GameObject.FindWithTag("GameManager") == null) { Instantiate(_GameManagerObject); }
+        _GameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
+
+        //Spaceキーが押されたときにLoadMainSceneを走らせる
+        this.UpdateAsObservable()
+            .Where(_ => Input.GetKeyDown(KeyCode.Space))
+            .Subscribe(_ => LoadMainScene());
+
+    }
+
+    //メインシーンに変移する
+    private void LoadMainScene()
+    {
+        _GameManager.ChangeScene(SceneType.Main);
+    }
+}
